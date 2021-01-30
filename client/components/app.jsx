938629a321312bc7c2bd8fe7f129423bdb370bfc
Daniel Godfrey <DanielGodfrey-Dev@outlook.com>
2021-01-30 03:20:21 -0800

    frontend React shell fully functional

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import axios from 'axios';
 
 class App extends React.Component {
-
-
-
-
-
-
-
-
-    
+    render() {
+        return  (
+            <h1>MEPHISTO IS HERE</h1>
+        )
+    }
 }
+
+export default App;
