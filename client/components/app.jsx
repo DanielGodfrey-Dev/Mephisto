769c0eee551682a8b593890c77a15420a490c8dc
Daniel Godfrey <DanielGodfrey-Dev@outlook.com>
2021-01-30 00:34:55 -0800

    successfully configured webpack, built entry point for application

---
+++ client/components/app.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+
+class App extends React.Component {
+
+
+
+
+
+
+
+
+    
+}
--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,51 @@ const SRC_DIR = path.join(__dirname, '/client');
 const DIST_DIR = path.join(__dirname, '/dist');
 
 module.exports = {
+    entry: `${SRC_DIR}/index.jsx`,
+    output: {
+        filename: 'bundle.js',
+        path: DIST_DIR
+    },
+    module: {
+        rules: [
+    //_________________beginning of rules____________________________________________
+            // used to implement CSS modules in React
+            {
+              test: /\.css$/,
+              include: SRC_DIR,
+              exclude: /node_modules/,
+              use: [
+                // [style-loader](/loaders/style-loader)
+                { loader: 'style-loader' },
+                // [css-loader](/loaders/css-loader)
+                {
+                  loader: 'css-loader',
+                  options: {
+                    modules: true
+                  }
+                }
+              ]
+            },
 
-
-
-
-
-
-
-}
+    //________________________________________________________________________________
+            // used for building in React (jsx -> js)
+            {
+                test: /\.(jsx|js)$/,
+                include: SRC_DIR,
+                exclude: /node_modules/,
+                use: [{
+                  loader: 'babel-loader',
+                  options: {
+                    presets: [
+                      ['@babel/preset-env', {
+                        "targets": "defaults" 
+                      }],
+                      '@babel/preset-react'
+                    ]
+                  }
+                }]
+            }
+    //_________________________end of rules____________________________________________
+        ]
+    }
+};
